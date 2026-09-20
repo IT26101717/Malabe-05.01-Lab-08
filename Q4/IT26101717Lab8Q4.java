5cc0bb7a773bf20eddc216547ec04c1501387929
IT26101717 <it26101717@my.sliit.lk>
2026-09-20 21:49:51 +0530

    Add files via upload

---
+++ Q4/IT26101717Lab8Q4.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class IT26101717Lab8Q4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] studentsArray = new int[8]; 
+        
+        for (int i = 0; i < studentsArray.length; ) {
+            System.out.print("Enter Student ID for Student " + (i + 1) + ": ");
+            int id = input.nextInt();
+
+            if (id > 0) {
+                studentsArray[i] = id; 
+                i++;                  
+            } else {
+                System.out.println("Error: Please Enter ONLY Positive Numbers");
+                // i not increment → same student repeat
+            }
+        }
+
+        
+        System.out.print("Enter a Student ID to Search: ");
+        int searchID = input.nextInt();
+
+        boolean found = false;
+        for (int i = 0; i < studentsArray.length; i++) {
+            if (studentsArray[i] == searchID) {
+                found = true;
+                break;
+            }
+        }
+
+        
+        if (found) {
+            System.out.println("Student is Available");
+        } else {
+            System.out.println("Student is Not Available");
+        }
+    }
+}
