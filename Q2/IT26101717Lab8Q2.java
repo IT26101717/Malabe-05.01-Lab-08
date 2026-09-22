003bdbcf9515a8c0a9e1deb2283de1f4aac12bee
IT26101717 <it26101717@my.sliit.lk>
2026-09-22 20:02:29 +0530

    Update IT26101717Lab8Q2.java

--- Q2/IT26101717Lab8Q2.java
+++ Q2/IT26101717Lab8Q2.java
@@ -3,8 +3,7 @@ public class IT26101717Lab8Q2 {
         
         int[] A = {10, 20, 30, 40, 50};
         int[] B = {34, 67, 12, 89, 12};
-        int[] C = new int[5]; // Result array
-
+        int[] C = new int[5]; 
         for (int i = 0; i < A.length; i++) {
             C[i] = A[i] + B[i];
         }
